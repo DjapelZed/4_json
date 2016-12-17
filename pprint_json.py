a10885f8177d4e7239adc3789a8fe62162cad0a7
DjapelZed <maks.kushnirov@gmail.com>
2016-12-17 23:39:51 +0300

    Pretty print(json)

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,21 @@
 import json
+import shutil
+import codecs
+import os
 
 
 def load_data(filepath):
-    pass
+    shutil.copy2(filepath, r'json-txt.txt')
+    json_to_parse = codecs.open('json-txt.txt', 'r', 'utf_8_sig')
+    parsed_json = json.loads(json_to_parse.read())
+    json_to_parse.close()
+    os.remove('file_json.txt')
+    return parsed_json
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data(input()))
